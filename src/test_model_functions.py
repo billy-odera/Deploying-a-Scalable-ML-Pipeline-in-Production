cc753b4aa8d66686da846412636371eb7d10ac13
Wei Chiet <weichiet@outlook.com>
2022-01-09 21:37:29 +0800

    Remove 'test_save_models()' and replace with two new unit tests

--- src/test_model_functions.py
+++ src/test_model_functions.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
+import sklearn
 from sklearn.model_selection import train_test_split
 from src.ml.data import process_data
-from src.ml.model import inference,load_models, cat_features
+from src.ml.model import inference,load_models, cat_features, compute_model_metrics
 
 def load_data():
     """
@@ -13,39 +14,45 @@ def load_data():
     trained_model, encoder, lb = load_models('model')
     _, test = train_test_split(data, test_size=0.20)
 
-    X_test, _, _, _ = process_data(
+    X_test, y_test, _, _ = process_data(
         test, categorical_features=cat_features, label="salary",
         encoder=encoder, lb=lb, training=False)
 
-    return data, trained_model, X_test
+    return data, trained_model, X_test, y_test
 
 def test_for_nulls():
     """
     Test to make sure there are no missing values in the cleaned_data
     """
-    data, _, _ = load_data()
+    data, _, _, _ = load_data()
     assert data.shape == data.dropna().shape
 
 def test_inference():
     """
     Test the type and value return by inference() is correct
     """
-    _, trained_model, X_test = load_data()
+    _, trained_model, X_test, _ = load_data()
     prediction = inference(trained_model, X_test)
 
     assert isinstance(prediction, np.ndarray)
     assert list(np.unique(prediction)) == [0, 1]
 
-def test_save_models(path="model"):
+def test_trained_model():
     """
-    Test the saved trained_model, encoder and lb files exists
+    Test the type of the trained_model is a GradientBoostingClassifier
     """
 
-    assert os.path.isfile(f"{path}/trained_model.joblib")
-    assert os.path.isfile(f"{path}/encoder.joblib")
-    assert os.path.isfile(f"{path}/lb.joblib")
+    _, trained_model, _, _= load_data()
+    assert type(trained_model) == sklearn.ensemble.GradientBoostingClassifier
 
+def test_compute_model_metrics():
+    """
+    Test the range of performance metrics returned by compute_model_metrics()
+    """
+    _, trained_model, X_test, y_test = load_data()
+    prediction = inference(trained_model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, prediction)
 
-test_for_nulls()
-test_inference()
-test_save_models()
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
